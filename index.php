ac7bec559194aeb9637dbf824ff7b34b5f10da70
Thibault Soquette <thibault.soquette.pro@gmail.com>
2023-12-10 22:05:08 +0100

    Add : objectives
    Signed-off-by: Thibault Soquette <thibault.soquette.pro@gmail.com>

--- index.php
+++ index.php
@@ -1,114 +1,111 @@
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link
-			rel="shortcut icon"
-			href="./assets/img/Synergie-Black-White.ico"
-			type="image/x-icon"
-		/>
-		<link rel="stylesheet" href="assets/css/style.css" />
-		<script
-			src="https://kit.fontawesome.com/28b70435c7.js"
-			crossorigin="anonymous"
-		></script>
-		<script src="./assets/js/main.js" defer></script>
-		<title>Document</title>
-	</head>
-	<body>
-		<header>
-			<a href="./"
-				><img
-					class="logo"
-					src="assets/img/Synergie-Black-White.png"
-					alt="Synergy logo"
-			/></a>
-			<nav class="nav-links">
-				<ul class="nav-list">
-					<li>
-						<a href="./"><i class="fa-solid fa-house"></i> Accueil</a>
-					</li>
-					<li>
-						<a href="./blog.php"
-							><i class="fa-solid fa-square-rss"></i> Blog</a
-						>
-					</li>
-					<li>
-						<a href="./ressources.php"
-							><i class="fa-solid fa-circle-info"></i> Ressources</a
-						>
-					</li>
-					<li>
-						<a href="./contact.html"
-							><i class="fa-regular fa-envelope"></i> Contact</a
-						>
-					</li>
-				</ul>
-			</nav>
-			<div class="burger-menu">
-				<div class="line1"></div>
-				<div class="line2"></div>
-				<div class="line3"></div>
-			</div>
-		</header>
-		<main>
-			<section id="home-text">
-				<h1>Qui sommes-nous ?</h1>
-				<p>
-					Synergie Don Bosco Vlaanderen-Wallonie est un projet de rencontre
-					interculturelle entre des élèves flamands et wallons.
-				</p>
-				<p>
-					Les élèves de diverses langues créeront un système aquaponique aligné
-					sur les objectifs durables des Nations Unies. En travaillant ensemble,
-					ils développeront des solutions novatrices pour réduire le changement
-					climatique, améliorer l'alimentation et prévenir de nouvelles
-					pandémies.
-				</p>
 
-				<p>
-					Dans un premier temps, nous ferons connaissance en ligne via des
-					activités proposées par les professeurs des trois écoles. Ces
-					activités permettront de partager sur la vie et la culture de chaque
-					communauté linguistique.
-				</p>
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="shortcut icon" href="./assets/img/Synergie-Black-White.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="assets/css/style.css" />
+	<script src="https://kit.fontawesome.com/28b70435c7.js" crossorigin="anonymous"></script>
+	<script src="./assets/js/main.js" defer></script>
+	<title>Document</title>
+</head>
 
-				<p>
-					Nous projetons également de nous rencontrer lors d'une grande journée
-					Don Bosco inter-régions.
-				</p>
+<body>
+	<header>
+		<a href="./"><img class="logo" src="assets/img/Synergie-Black-White.png" alt="Synergy logo" /></a>
+		<nav class="nav-links">
+			<ul class="nav-list">
+				<li>
+					<a href="./"><i class="fa-solid fa-house"></i> Accueil</a>
+				</li>
+				<li>
+					<a href="./blog.php"><i class="fa-solid fa-square-rss"></i> Blog</a>
+				</li>
+				<li>
+					<a href="./ressources.php"><i class="fa-solid fa-circle-info"></i> Ressources</a>
+				</li>
+				<li>
+					<a href="./contact.html"><i class="fa-regular fa-envelope"></i> Contact</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="burger-menu">
+			<div class="line1"></div>
+			<div class="line2"></div>
+			<div class="line3"></div>
+		</div>
+	</header>
+	<main>
+		<section id="home-text">
+			<h1>Qui sommes-nous ?</h1>
+			<p>
+				Synergie Don Bosco Vlaanderen-Wallonie est un projet de rencontre
+				interculturelle entre des élèves flamands et wallons.
+			</p>
+			<p>
+				Les élèves de diverses langues créeront un système aquaponique aligné
+				sur les objectifs durables des Nations Unies. En travaillant ensemble,
+				ils développeront des solutions novatrices pour réduire le changement
+				climatique, améliorer l'alimentation et prévenir de nouvelles
+				pandémies.
+			</p>
 
-				<p>
-					Ensuite, les élèves de 4e année de l'enseignement général, option
-					sciences fortes du campus Sint-Pieters de Gand commenceront à se
-					documenter sur le système aquaponique pour pouvoir l'expliquer aux
-					élèves des autres écoles.
-				</p>
+			<p>
+				Dans un premier temps, nous ferons connaissance en ligne via des
+				activités proposées par les professeurs des trois écoles. Ces
+				activités permettront de partager sur la vie et la culture de chaque
+				communauté linguistique.
+			</p>
 
-				<p>
-					Les élèves de Don Bosco Halle et Don Bosco Remouchamps, ayant reçu les
-					explications de Sint-Pieters, pourront envisager la réalisation du
-					système. Les écoles techniques créeront un premier projet, présenté
-					aux élèves de sciences pour leur analyse et suggestions
-					d'amélioration.
-				</p>
+			<p>
+				Nous projetons également de nous rencontrer lors d'une grande journée
+				Don Bosco inter-régions.
+			</p>
 
-				<p>
-					Enfin, les élèves de Don Bosco Remouchamps iront placer ce système à
-					Malte et pourront donner un retour aux deux autres écoles partenaires
-					sur la mise en œuvre du projet.
-				</p>
-			</section>
-		</main>
-		<footer>
-			Copyright &copy; 2023-2024
-			<a
-				href="https://www.saint-raphael.be/"
-				target="_blank"
-				rel="noopener noreferrer"
-				>Saint-Raphael Remouchamps</a
-			>. Tous droits réservés.
-		</footer>
-	</body>
+			<p>
+				Ensuite, les élèves de 4e année de l'enseignement général, option
+				sciences fortes du campus Sint-Pieters de Gand commenceront à se
+				documenter sur le système aquaponique pour pouvoir l'expliquer aux
+				élèves des autres écoles.
+			</p>
+
+			<p>
+				Les élèves de Don Bosco Halle et Don Bosco Remouchamps, ayant reçu les
+				explications de Sint-Pieters, pourront envisager la réalisation du
+				système. Les écoles techniques créeront un premier projet, présenté
+				aux élèves de sciences pour leur analyse et suggestions
+				d'amélioration.
+			</p>
+
+			<p>
+				Enfin, les élèves de Don Bosco Remouchamps iront placer ce système à
+				Malte et pourront donner un retour aux deux autres écoles partenaires
+				sur la mise en œuvre du projet.
+			</p>
+		</section>
+		<section id="obj">
+			<h2>Objectifs du projet :</h2>
+			<ul>
+				<li>Rencontre interculturelle Flandre-Wallonie</li>
+				<li>Amélioration des compétences linguistiques</li>
+				<li>Réflexion sur des modes de production alimentaire plus durables</li>
+				<li>Innovation technologique</li>
+				<li>Développement des compétences de collaboration et d'entraide</li>
+				<li>Familiarisation avec les outils numériques</li>
+				<li>Formation à la tolérance</li>
+				<li>Réflexion sur ce qu'est une alimentation saine et durable</li>
+				<li>Développement de compétences numériques</li>
+				<li>Développement de la collaboration entre élèves de différentes filières (techniques de scientifique).
+				</li>
+			</ul>
+		</section>
+	</main>
+	<footer>
+		Copyright &copy; 2023-2024
+		<a href="https://www.saint-raphael.be/" target="_blank" rel="noopener noreferrer">Saint-Raphael Remouchamps</a>.
+		Tous droits réservés.
+	</footer>
+</body>
+
 </html>
