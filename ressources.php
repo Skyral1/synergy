97f17eef45bce71b63c1801af6986d6e770d55c9
Thibault Soquette <thibault.soquette.pro@gmail.com>
2023-12-26 17:34:17 +0100

    Ressources...
    Signed-off-by: Thibault Soquette <thibault.soquette.pro@gmail.com>

--- ressources.php
+++ ressources.php
@@ -1,2 +1,77 @@
-<!-- Dev by @Skyral_ -->
-<!-- https://github.com/Skyral1 -->
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="shortcut icon" href="./assets/img/Synergie-Black-White.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="assets/css/style.css" preload />
+	<script src="https://kit.fontawesome.com/28b70435c7.js" crossorigin="anonymous" defer></script>
+	<script src="./assets/js/main.js" defer></script>
+	<!-- Dev by @Skyral_ -->
+	<!-- https://github.com/Skyral1 -->
+	<title>Document</title>
+</head>
+
+<body>
+	<header>
+		<a href="./"><img class="logo" src="assets/img/Synergie-Black-White.webp" alt="Synergy logo" /></a>
+		<nav class="nav-links">
+			<ul class="nav-list">
+				<li>
+					<a href="./"><i class="fa-solid fa-house" style="color: #ffffff;"></i> Accueil</a>
+				</li>
+				<li>
+					<a href="./blog.php"><i class="fa-solid fa-square-rss" style="color: #ffffff;"></i> Blog</a>
+				</li>
+				<li>
+					<a href="./ressources.php"><i class="fa-solid fa-circle-info" style="color: #ffffff;"></i>
+						Ressources</a>
+				</li>
+				<li>
+					<a href="./contact.html"><i class="fa-regular fa-envelope" style="color: #ffffff;"></i> Contact</a>
+				</li>
+			</ul>
+		</nav>
+		<div class="burger-menu">
+			<div class="line1"></div>
+			<div class="line2"></div>
+			<div class="line3"></div>
+		</div>
+		<ul class="burger-list">
+			<li>
+				<a href="./"><i class="fa-solid fa-house" style="color: #ffffff;"></i> Accueil</a>
+			</li>
+			<li>
+				<a href="./blog.php"><i class="fa-solid fa-square-rss" style="color: #ffffff;"></i> Blog</a>
+			</li>
+			<li>
+				<a href="./ressources.php"><i class="fa-solid fa-circle-info" style="color: #ffffff;"></i>
+					Ressources</a>
+			</li>
+			<li>
+				<a href="./contact.html"><i class="fa-regular fa-envelope" style="color: #ffffff;"></i> Contact</a>
+			</li>
+		</ul>
+	</header>
+	<main>
+		<section id="home-text">
+			<h1>Page en construction</h1>
+            <p>À Bientôt!</p>
+		</section>
+	</main>
+	<footer>
+		<div class="footer-content">
+			Copyright &copy; 2023-2024
+			<a href="https://www.saint-raphael.be/" target="_blank" rel="noopener noreferrer">Saint-Raphaël
+				Remouchamps</a>
+			Tous droits réservés.
+			<a href="https://www.facebook.com/profile.php?id=61551151826736" target="_blank" rel="noopener noreferrer">
+				<i class="fa-brands fa-square-facebook fa-lg" style="color: #ffffff;"></i>
+			</a>
+		</div>
+	</footer>
+
+</body>
+
+</html>
